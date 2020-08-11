06925dfca40152c59da7c014c07ace0cd30622c6
Killamsetty <vinesh.killamsetty@dxc.com>
2020-08-11 17:18:54 +0530

    first commit

---
+++ Circle.java
@@ -0,0 +1,12 @@
+public class Circle
+{
+   double radius;
+   public Circle(double radius)
+   {	
+      this.radius = radius;
+   }
+   public double area()
+   {
+     return 2*radius*radius;
+   }
+}
